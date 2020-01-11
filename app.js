af8e411944b2e7e13163c0fd1e9d15aa6e9ea4b2
Boubou <Elodie.morin1298@gmail.com>
2020-01-11 10:23:08 +0000

    <NV><Find conversation for a given user endpoint>

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ let app = express();
 
 let user = require('./app/database/user');
 
-app.get("/url", (req, res, next) => {
+app.get("/url", (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
     user.findAll(res);
+});
+
+app.get("/conversations", (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    if (req.query.id_user != null) {
+        user.getConversations(res, req.query.id_user);
+    }
+    else {
+        res.status(400);
+        res.send('You should specify the id_user parameter.');
+    }
 });
 
 app.listen(3000, () => {
--- app/database/user.js
+++ app/database/user.js
@@ -7,6 +7,20 @@ function findAll(res) {
     });
 }
 
+function getConversations(res, id_user) {
+    return connection.query(
+        `SELECT conversation.id_conversation, title, surname, name, content, timestamp 
+        FROM conversation NATURAL JOIN participant NATURAL JOIN user LEFT JOIN message 
+        ON conversation.id_conversation = message.id_conversation AND participant.id_user = message.id_user 
+        WHERE conversation.id_conversation IN (SELECT id_conversation FROM participant WHERE id_user = ${id_user})`,
+        (err, rows, _) => {
+            if (err) throw err;
+            res.json(rows);
+        }
+    )
+}
+
 module.exports = {
-    findAll
+    findAll,
+    getConversations
 };
