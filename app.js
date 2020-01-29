119123b027523929bcd4e43677dfa54e6ac15b40
Boubou <Elodie.morin1298@gmail.com>
2020-01-29 17:47:28 +0100

    <NV><User registration function>

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let express = require("express");
+let bodyParser = require("body-parser");
+
 let app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 let user = require('./app/database/user');
 
@@ -16,6 +19,17 @@ app.get("/conversations", (req, res) => {
     else {
         res.status(400);
         res.send('You should specify the id_user parameter.');
+    }
+});
+
+app.all('/user', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    console.log(req.body);
+    if (req.method === 'POST') {
+        user.getUser(res, req.body.name, req.body.phoneNumber);
+    } else {
+        res.status(400);
+        res.send('Invalid request, this method isn\'t allowed.');
     }
 });
 
--- app/database/user.js
+++ app/database/user.js
@@ -9,7 +9,7 @@ function findAll(res) {
 
 function getConversations(res, id_user) {
     return connection.query(
-        `SELECT conversation.id_conversation, title, surname, name, content, timestamp 
+        `SELECT conversation.id_conversation, title, participant.id_user, surname, name, content, timestamp 
         FROM conversation NATURAL JOIN participant NATURAL JOIN user LEFT JOIN message 
         ON conversation.id_conversation = message.id_conversation AND participant.id_user = message.id_user 
         WHERE conversation.id_conversation IN (SELECT id_conversation FROM participant WHERE id_user = ${id_user})`,
@@ -20,7 +20,31 @@ function getConversations(res, id_user) {
     )
 }
 
+function getUser(res, name, phoneNumber) {
+    return new Promise(resolve => {
+        connection.query(`SELECT id_user FROM user WHERE phone_number='${phoneNumber}'`,
+            (err, rows, _) => {
+                if (err) throw err;
+                if (rows[0]) {
+                    res.json(rows[0].id_user);
+                    resolve(true);
+                }
+                resolve(false);
+            });})
+        .then(answer => new Promise(resolve => {
+            if (!answer) {
+                connection.query(`INSERT INTO user (name, phone_number) VALUES ('${name}', '${phoneNumber}')`,
+                    (err, rows, _) => {
+                        if (err) throw err;
+                        res.json(rows.insertId);
+                    })
+            }
+            resolve(true);
+        }));
+}
+
 module.exports = {
     findAll,
-    getConversations
+    getConversations,
+    getUser
 };
