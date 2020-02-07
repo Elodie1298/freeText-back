12332442a07221fe5957c4083e7b024cbc3a1df3
Boubou <Elodie.morin1298@gmail.com>
2020-02-07 09:35:27 +0000

    <UP><Add get user endpoint>

--- app/database/user.js
+++ app/database/user.js
@@ -1,18 +1,28 @@
-let util = require('../util');
+
+
+// Get user from id
+import {executeQuery} from "../util";
+
+
+// Recover user from id
+export function get(id_user) {
+    let query = `select * from user where id_user = ${id_user}`;
+    return executeQuery(query);
+}
 
 
 // Recover user id based on its informations
 export function login(name, phoneNumber) {
     return new Promise((resolve, reject) => {
         let queryGet = `SELECT id_user FROM user WHERE phone_number='${phoneNumber}'`;
-        util.executeQuery(queryGet)
+        executeQuery(queryGet)
             .then(rows => {
                 if (rows[0]) {
                     resolve(rows[0])
                 } else {
                     let queryInsert = `INSERT INTO user (name, phone_number) VALUES
                                        ('${name}', '${phoneNumber}')`;
-                    return util.executeQuery(queryInsert);
+                    return executeQuery(queryInsert);
                 }
             })
             .then(rows => {
