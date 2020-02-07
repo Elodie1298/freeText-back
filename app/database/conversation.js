f36143bbdf182c8205b5414fa8802d850751e1c4
Boubou <Elodie.morin1298@gmail.com>
2020-02-07 14:26:57 +0000

    <UP><Manage get request using timestamp>

--- app/database/conversation.js
+++ app/database/conversation.js
@@ -2,9 +2,12 @@ import { timestampToString, executeQuery } from "../util";
 
 
 // Recover all conversations for a given user
-export function get(id_user) {
-    let query = `SELECT conversation.id_conversation, conversation.title FROM conversation 
+export function get(id_user, timestamp) {
+    let query = `SELECT conversation.* FROM conversation 
                  NATURAL JOIN participant WHERE id_user=${id_user}`;
+    if (timestamp != null) {
+        query += ` and conversation.timestamp > '${timestampToString(timestamp)}'`;
+    }
     return executeQuery(query);
 }
 
