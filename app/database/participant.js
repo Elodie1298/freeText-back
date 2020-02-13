3c1ee783be7ee83b45c30b67ca25c8af4c741ae0
Boubou <Elodie.morin1298@gmail.com>
2020-02-13 09:13:37 +0000

    <FIX><Insert participant query>

--- app/database/participant.js
+++ app/database/participant.js
@@ -5,8 +5,7 @@ let executeQuery = require("../util").executeQuery;
 // Recover all participants for a given user's conversations
 function get(id_user, timestamp) {
     let query = `select p2.* from participant as p1 join participant as p2 on
-                 p1.id_conversation = p2.id_conversation where p1.id_user != p2.id_user
-                 and p1.id_user = ${id_user}`;
+                 p1.id_conversation = p2.id_conversation where p1.id_user = ${id_user}`;
     if (timestamp != null) {
         query += ` and p2.timestamp > '${timestamp}'`;
     }
@@ -18,7 +17,7 @@ function get(id_user, timestamp) {
 function add(id_user, id_conversation, nickname, timestamp) {
     let query = `insert into participant(id_user, id_conversation, nickname, timestamp)
                  values (${id_user}, ${id_conversation}, ${nickname ? nickname : 'NULL'},
-                 ${timestamp})`;
+                 '${timestamp}')`;
     return executeQuery(query)
         .then(result => new Promise(resolve => resolve(result.insertId)));
 }
